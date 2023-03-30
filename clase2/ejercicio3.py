f01ec0f1ca3bdb5bfa2c4c68dd980215f9522243
Fernando Isaguirre <fernando.isaguirre.13@gmail.com>
2023-03-30 13:09:14 -0300

    Ejercicio 3 - manejo de errores

--- clase2/ejercicio3.py
+++ clase2/ejercicio3.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-import getopt
 import argparse
 import os
 import sys
-
-"""
-Escribir un programa en Python que acepte argumentos de línea de comando para leer un archivo de texto (pasado como argumento). El programa debe contar el número de palabras y líneas del archivo e imprimirlas en la salida estándar. Además el programa debe aceptar una opción para imprimir la longitud promedio de las palabras del archivo. Esta última opción no debe ser obligatoria. Si hubiese errores deben guardarse el un archivo cuyo nombre será "errors.log" usando la redirección de la salida de error.
-
-La opcion no es obligatoria en el sentido de que puedo o no ponerla en el bash, pero debo programarlo.
-"""
 
 
 def ArgsParse():
@@ -16,43 +9,55 @@ def ArgsParse():
     parser.add_argument("-f", "--file", dest="archivo",
                         required=True, help="Nombre del archivo")
 
-    parser.add_argument("-l", "--longitud", dest="longitudprom", required=False, nargs="?",
+    parser.add_argument("-l", "--longitud", dest="longitudprom", required=False,  action="store_true",
                         help="Longitud promedio de palabras")
 
     return parser.parse_args()
 
 
-def LeerArchivo(nombreArchivo):
-    fd = os.open(nombreArchivo, os.O_RDONLY)
-    # Contar cantidad de palabras
-    texto = os.read(fd, 5000)
-    palabras = texto.split()
+def LeerArchivo(nombreArchivo, condicionLongitud):
+    try:
+        fd = os.open(nombreArchivo, os.O_RDONLY)
+        # Contar cantidad de palabras
+        texto = os.read(fd, 5000)
+        palabras = texto.split()
 
-    # Contar cantidad de lineas
-    lines = texto.splitlines()
+        # Contar cantidad de lineas
+        lines = texto.splitlines()
 
-    # Longitud promedio
-    caracteres_totales = 0
-    numero_palabra = 0
-    final = len(palabras)
-    while True:
-        caracteres_totales = caracteres_totales + len(palabras[numero_palabra])
-        numero_palabra = numero_palabra + 1
-        if numero_palabra == final:
-            break
-    longitud_promedio = caracteres_totales/len(palabras)
+        # Longitud promedio - verificamos la condicion (True/False) para sacar la longitud promedio
+        if condicionLongitud:
+            caracteres_totales = 0
+            numero_palabra = 0
+            final = len(palabras)
+            while True:
+                caracteres_totales = caracteres_totales + \
+                    len(palabras[numero_palabra])
+                numero_palabra = numero_palabra + 1
+                if numero_palabra == final:
+                    break
+            longitud_promedio = caracteres_totales/len(palabras)
 
-    lista = [len(palabras), len(lines), longitud_promedio]
-    return lista
+            resultados = [len(palabras), len(lines), longitud_promedio]
+        else:
+            resultados = [len(palabras), len(lines), False]
+        return resultados
+
+    except OSError as error:
+        with open('errors.log', mode='w+') as errorFile:
+            print(error, file=errorFile)
+            sys.stdout.write('\n')
 
 
 if __name__ == "__main__":
     argumento = ArgsParse()
     nombreArchivo = argumento.archivo
-    lista = LeerArchivo(nombreArchivo)
-    cantidad_palabras = lista[0]
-    cantidad_lineas = lista[1]
-    longitud_prom = lista[2]
+    condicionLongitud = argumento.longitudprom
+
+    resultados = LeerArchivo(nombreArchivo, condicionLongitud)
+    cantidad_palabras = resultados[0]
+    cantidad_lineas = resultados[1]
+    longitud_prom = resultados[2]
     sys.stdout.write("\n")
     sys.stdout.write(
         "Cantidad de palabras que contiene el archivo: " + str(cantidad_palabras))
@@ -60,6 +65,7 @@ if __name__ == "__main__":
     sys.stdout.write(
         "Cantidad de lineas que contiene el archivo: " + str(cantidad_lineas))
     sys.stdout.write("\n")
-    sys.stdout.write(
-        "Longitud promedio de palabras que contiene el archivo: " + str(longitud_prom))
-    sys.stdout.write("\n")
+    if condicionLongitud:
+        sys.stdout.write(
+            "Longitud promedio de palabras que contiene el archivo: " + str(longitud_prom))
+        sys.stdout.write("\n")
