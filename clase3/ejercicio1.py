e1e95b09088b9a3152b6f2ae96556b0db68fceb1
Fernando Isaguirre <fernando.isaguirre.13@gmail.com>
2023-03-30 13:52:31 -0300

    Ejercicio 1
    Proceso padre calcula las raices si no hay hijo, caso contrario cada uno calcula su raiz.

---
+++ clase3/ejercicio1.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import os
+import argparse
+import sys
+import time
+
+
+def ArgsParse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--number", dest="numero",
+                        required=True, help="Numero")
+    parser.add_argument("-f", "--fork", dest="fork",
+                        required=False,  action="store_true")
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    argumento = ArgsParse()
+    numero = int(argumento.numero)
+    proceso = argumento.fork
+
+    sys.stdout.write("\n"+"Soy el padre: "+str(os.getpid()))
+    raizPositiva = pow(numero, 1/2)
+    sys.stdout.write("\n"+"La raiz positiva de " +
+                     str(numero)+" es: "+str(raizPositiva)+"\n")
+
+    if (proceso):
+        process = os.fork()
+        if process == 0:
+            sys.stdout.write("\n")
+            sys.stdout.write("Soy el hijo "+str(os.getpid()) +
+                             " y mi padre es: "+str(os.getppid()))
+            sys.stdout.write("\n")
+            raizNegativa = pow(numero, 1/(-2))
+            sys.stdout.write("La raiz negativa de " +
+                             str(numero)+" es: "+str(raizNegativa)+"\n")
+    else:
+        sys.stdout.write("\n"+"Soy un padre sin hijos")
+        sys.stdout.write("\n"+"Soy el padre: "+str(os.getpid()))
+        raizNegativa = pow(numero, 1/(-2))
+        sys.stdout.write("\n"+"La raiz negativa de " +
+                         str(numero)+" es: "+str(raizNegativa)+"\n")
