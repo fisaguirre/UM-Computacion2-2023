dc0ab6a316908f3357c1d24195bee1a7ed495cdc
Fernando Isaguirre <fernando.isaguirre.13@gmail.com>
2023-09-05 11:28:04 -0300

    Clase_14-EJercicio

--- Clases/Clase_14/ejercicio/ejercicio.py
+++ Clases/Clase_14/ejercicio/ejercicio.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
-import socket, sys, time
+"""
+Escribir un programa que implemente un socket pasivo que gestione de forma serializada distintas conecciones entrantes.
+
+Debe atender nuevas conexiones de forma indefinida.
+
+NOTA: cuando decimos serializado decimo que atiende una conexión y 
+recibe una nueva conección una vez que esa conexión se cerró
+"""
+
+import socket
 
 # create a socket object
-serversocket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM) 
-serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+#serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 """
     socket.AF_INET -> sockets tcp/ip
     socket.AF_UNIX -> sockets Unix (archivos en disco, similar a FIFO/named pipes)
@@ -12,26 +21,23 @@ serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     socket.SOCK_DGRAM -> socket udp, datagrama de usuario (no orientado a la conexion)
 """
 
-# get local machine name
-host = ""
-port = int(sys.argv[1])
+host = "0.0.0.0"
+port = 50010
 
-# bind to the port
-serversocket.bind((host, port))                                  
+serversocket.bind(('0.0.0.0',50011))                                  
 
-# queue up to 5 requests
 serversocket.listen(1)
 
 while True:
-    # establish a connection
+    print("\n")
     print("Esperando conexiones remotas (accept)")
     clientsocket,addr = serversocket.accept()      
 
-    print("Got a connection from %s" % str(addr))
-    
+    print("Conexión desde %s" % str(addr))
     msg = 'Hola Mundo'+ "\r\n"
     #clientsocket.send(msg.encode('ascii'))
     print("Enviando mensaje...")
+
     clientsocket.send(msg.encode('utf-8'))
     print("Cerrando conexion...")
     clientsocket.close()
